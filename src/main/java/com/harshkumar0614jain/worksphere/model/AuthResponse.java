9fbc80ae500dfc92346884ab19a15a71467e4750
harsh061402 <harshkumar0614jain@gmail.com>
2026-04-11 05:28:30 +0530

    feat(auth): add AuthResponse model for JWT token response

---
+++ src/main/java/com/harshkumar0614jain/worksphere/model/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.harshkumar0614jain.worksphere.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
