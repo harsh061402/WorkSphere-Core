b2c4961d5feb7c30a1dffe8c825cf89d72b2e22a
harsh061402 <harshkumar0614jain@gmail.com>
2026-04-11 01:29:40 +0530

    feat(auth): add JwtService for token generation and validation

---
+++ src/main/java/com/harshkumar0614jain/worksphere/service/JwtService.java
@@ -0,0 +1,68 @@
+package com.harshkumar0614jain.worksphere.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    // Called during LOGIN — generates token for user
+    public String generateToken(String username){
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+ expiration))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    //  Called in JwtAuthFilter — reads who this token belongs to
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // Called in JwtAuthFilter — checks token is valid + not expired
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()))
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return resolver.apply(claims);
+    }
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
